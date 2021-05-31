854a22c893536c7f647e83f34bb87e0a547b9b33
tipuusultan <62797976+tipuusultan@users.noreply.github.com>
2021-05-31 15:19:06 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,7 +9,7 @@ group_id = '-543656097'
 
 
 def fectch_data():
-    tele_api_url = f'https://api.telegram.org/bot1789281332:AAGvv5r-ZSC4CM11yrYeDQkw93H7KpHTMP8/sendMessage?chat_id={group_id}&text="massage"'        
+    tele_api_url = f'https://api.telegram.org/bot1789281332:AAEcL55TaDKQs3pSiQYN1h_FoRDmB-kdIOo/sendMessage?chat_id={group_id}&text="massage"'        
     tele_response = requests.get(tele_api_url)
     print("ok")
 
