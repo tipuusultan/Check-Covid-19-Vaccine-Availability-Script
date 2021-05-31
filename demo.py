63d31bd25a8f45b58e83c5c0c013c1ddb47378dd
tipuusultan <62797976+tipuusultan@users.noreply.github.com>
2021-05-31 15:50:29 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,16 +2,23 @@
 
 import requests
 
+base_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict"
+dist_id = 49
+date = "31-05-2021"
 
 # Send to telegram
 group_id = '-543656097'
 
 
 
-def fectch_data():
-    tele_api_url = f'https://api.telegram.org/bot1789281332:AAEcL55TaDKQs3pSiQYN1h_FoRDmB-kdIOo/sendMessage?chat_id={group_id}&text="massage"'        
-    tele_response = requests.get(tele_api_url)
-    print("ok")
-
 while True:
-    fectch_data()
+    final_url = f'{base_url}?district_id={dist_id}&date={date}'
+    response = requests.get(final_url)
+    json_response = response.json()
+    for center in json_response['sessions']:
+        print("Checking Vaccine Slots......")
+        if center['available_capacity_dose1'] > 0:
+            massage = f"Center name: {center['name']} \n Pincode: {center['pincode']} \n Dose1: {center['available_capacity_dose1']} \n Age: {center['min_age_limit']}"
+            tele_api_url = f'https://api.telegram.org/bot1789281332:AAEcL55TaDKQs3pSiQYN1h_FoRDmB-kdIOo/sendMessage?chat_id={group_id}&text={massage}'
+            tele_response = requests.get(tele_api_url)
+            print(massage)
