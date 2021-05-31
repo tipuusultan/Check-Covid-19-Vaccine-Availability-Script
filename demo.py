7da43d8a46e13897368e042d126ae08e10ee0b26
tipuusultan <62797976+tipuusultan@users.noreply.github.com>
2021-05-31 16:03:52 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -12,8 +12,11 @@ group_id = '-543656097'
 
 
 while True:
+    base_request_header = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+            }
     final_url = f'{base_url}?district_id={dist_id}&date={date}'
-    response = requests.get(final_url)
+    response = requests.get(final_url , headers=base_request_header)
     print(response)
     print(final_url)
     print(response.text)
