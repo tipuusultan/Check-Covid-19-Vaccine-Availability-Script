c277a27f023e6391fb0499aa80b76f6a3236e0f7
tipuusultan <62797976+tipuusultan@users.noreply.github.com>
2021-05-31 15:53:47 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -15,10 +15,4 @@ while True:
     final_url = f'{base_url}?district_id={dist_id}&date={date}'
     response = requests.get(final_url)
     json_response = response.json()
-    for center in json_response['sessions']:
-        print("Checking Vaccine Slots......")
-        if center['available_capacity_dose1'] > 0:
-            massage = f"Center name: {center['name']} \n Pincode: {center['pincode']} \n Dose1: {center['available_capacity_dose1']} \n Age: {center['min_age_limit']}"
-            tele_api_url = f'https://api.telegram.org/bot1789281332:AAEcL55TaDKQs3pSiQYN1h_FoRDmB-kdIOo/sendMessage?chat_id={group_id}&text={massage}'
-            tele_response = requests.get(tele_api_url)
-            print(massage)
+    print(json_response)
