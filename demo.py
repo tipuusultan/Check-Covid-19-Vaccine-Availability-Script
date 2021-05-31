1008b28e0967289397e56520b6b033e2b0ba8c04
tipuusultan <62797976+tipuusultan@users.noreply.github.com>
2021-05-31 16:13:37 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,12 +11,12 @@ group_id = '-543656097'
 
 
 
-while True:
-    base_request_header = {
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
-            }
-    final_url = f'{base_url}?district_id={dist_id}&date={date}'
-    response = requests.get(final_url , headers=base_request_header)
-    print(response)
-    print(final_url)
-    print(response.text)
+
+base_request_header = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
+        }
+final_url = f'{base_url}?district_id={dist_id}&date={date}'
+response = requests.get(final_url , headers=base_request_header)
+print(response)
+print(final_url)
+print(response.text
